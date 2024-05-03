e6899af14e923b16822243a4fd9d021826130fb5
Ocean Briggs-Mineart <Forestbriggs14@gmail.com>
2024-05-02 23:55:16 -0700

    styled Navigation component. added dropdown menu with needed event listeners. Finished Navigation component;

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,36 +1,45 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Navigate, useNavigate } from 'react-router-dom';
 import './Navigation.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../../store/session';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 
 export default function Navigation() {
-    const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
+    const navigate = useNavigate();
 
-    const handleLogout = (e) => {
-        e.preventDefault();
-        dispatch(logout());
+    const handleClick = () => {
+        return navigate('/signup')
+    }
+
+    const handleNameClick = () => {
+        return navigate('/')
     }
 
     return (
-        <ul className='NavBar'>
-            <NavLink to="/">Home</NavLink>
-            {
-                //* Renders when not logged in
-            }
-            {!sessionUser && <>
-                <NavLink to="login">Login</NavLink>
-                <NavLink to="signup">Sign Up</NavLink>
-            </>}
+        <ul className='nav-container'>
+            <span onClick={handleNameClick}>Grand Line Gatherings</span>
+            <nav className='NavBar'>
+                <NavLink to="/">Home</NavLink>
+                {
+                    //* Renders when not logged in
+                }
+                {!sessionUser && <>
+                    <NavLink to="login">Login</NavLink>
+                    <button
+                        className='signup-button'
+                        onClick={handleClick}
+                    >
+                        Sign Up
+                    </button>
+                </>}
 
-            {
-                //* Renders when logged in
-            }
-            {sessionUser && <>
-                <ProfileButton user={sessionUser} />
-                <button onClick={handleLogout}>Logout</button>
-            </>}
+                {
+                    //* Renders when logged in
+                }
+                {sessionUser && <>
+                    <ProfileButton user={sessionUser} />
+                </>}
+            </nav>
         </ul>
     )
 }
